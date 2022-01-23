2ef144201634405bc3309de2579732ff51df2e42
joshlyons225 <95392565+joshlyons225@users.noreply.github.com>
2022-01-23 12:53:17 -0600

    updated loadTasks function

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,16 +20,39 @@ $(document).ready(function() {
     // create loadTasks function to check from localStorage
     var loadTasks = function() {
         tasks = JSON.parse(localStorage.getItem(time, task));
+
+        // retrieve saved data for all hours from localStorage
+        $("#hour-8 .description").val(localStorage.getItem("hour-8"));
     }
 
-    // retrieve current time
-    var currentTime = moment().hour();
-    console.log(currentTime);
+    // function to set appropriate time naming conventions
+    var timeClassUpdate = function() {
+
+        // retrieve current time
+        var currentTime = moment().hour();
+        console.log(currentTime);
     
-    // create function to loop and assign past, present, future classes
-
-
+        // create function to loop and assign past, present, future classes
+        $(".time-block").each(function() {
+            var tense = parseInt($(this).attr("id").split("hour")[1]);
+            
+            if (tense < currentTime) {
+                $(this).removeClass("present");
+                $(this).removeClass("future");
+                $(this).addClass("past");
+            } else if (tense === currentTime) {
+                $(this).removeClass("past");
+                $(this).removeClass("future");
+                $(this).addClass("present");
+            } else {
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+                $(this).addClass("future");
+            }
+        })
+    }
 
     // callbacks
     loadTasks();
+    timeClassUpdate();
 })
