25ca510129902e6f0a1f00a0a9f8ca01cd00ed30
joshlyons225 <95392565+joshlyons225@users.noreply.github.com>
2022-01-23 10:22:24 -0600

    added current time retrieval js

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,5 +6,19 @@ $("#currentDay").html(currentDate);
 $(document).ready(function() {
     $(".saveBtn").on("click", function() {
         console.log("button clicked");
+
+        // create var def for time
+        // create var def for task description
+
+        // add to localStorage
+        localStorage.setItem(JSON.stringify(time, task));
     })
+
+    // create loadTasks function to check from localStorage
+
+    // retrieve current time
+    var currentTime = moment().hour();
+    console.log(currentTime);
+    
+    // create function to loop and assign past, present, future classes
 })
