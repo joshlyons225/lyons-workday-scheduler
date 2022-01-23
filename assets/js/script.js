779028e25e9c2bace9e4410bee3ebb8b55c64605
joshlyons225 <95392565+joshlyons225@users.noreply.github.com>
2022-01-23 09:28:22 -0600

    added initial saveBtn function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,10 @@
 // today's date global var 
 var currentDate = moment().format('dddd, MMM Do YYYY');
 $("#currentDay").html(currentDate);
+
+// saveBtn function
+$(document).ready(function() {
+    $(".saveBtn").on("click", function() {
+        console.log("button clicked");
+    })
+})
