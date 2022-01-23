bb67ad7ab1a12c532e79785f7c26571d6ae22ed0
joshlyons225 <95392565+joshlyons225@users.noreply.github.com>
2022-01-23 12:57:03 -0600

    added all single hours to loadTask function

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,6 +23,15 @@ $(document).ready(function() {
 
         // retrieve saved data for all hours from localStorage
         $("#hour-8 .description").val(localStorage.getItem("hour-8"));
+        $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+        $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+        $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+        $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+        $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+        $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+        $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+        $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+        $("#hour-17 .description").val(localStorage.getItem("hour-17"));
     }
 
     // function to set appropriate time naming conventions
@@ -35,7 +44,7 @@ $(document).ready(function() {
         // create function to loop and assign past, present, future classes
         $(".time-block").each(function() {
             var tense = parseInt($(this).attr("id").split("hour")[1]);
-            
+
             if (tense < currentTime) {
                 $(this).removeClass("present");
                 $(this).removeClass("future");
