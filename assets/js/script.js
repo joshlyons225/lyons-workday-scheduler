4108c8aa641d2c8e89b80ab559894eb4f1806b81
joshlyons225 <95392565+joshlyons225@users.noreply.github.com>
2022-01-23 12:32:15 -0600

    added var definitions for time and task, added loadTask from localStorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,17 +8,28 @@ $(document).ready(function() {
         console.log("button clicked");
 
         // create var def for time
+        var time = $(this).parent().attr("id");
+
         // create var def for task description
+        var task = $(this).siblings(".description").val();
 
         // add to localStorage
         localStorage.setItem(JSON.stringify(time, task));
     })
 
     // create loadTasks function to check from localStorage
+    var loadTasks = function() {
+        tasks = JSON.parse(localStorage.getItem(time, task));
+    }
 
     // retrieve current time
     var currentTime = moment().hour();
     console.log(currentTime);
     
     // create function to loop and assign past, present, future classes
+
+
+
+    // callbacks
+    loadTasks();
 })
